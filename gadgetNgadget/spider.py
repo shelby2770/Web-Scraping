67245611e75d0c8f437eb0091cb0c0a0e7347fb0
Adnan Istiaque <ajm.istiaque@g.bracu.ac.bd>
2023-11-30 20:54:35 +0600

    gadgetmonkey discount price added

--- gadgetNgadget/spider.py
+++ gadgetNgadget/spider.py
@@ -66,10 +66,11 @@ class Spider3(scrapy.Spider):
         shop_img = response.css('.h-md-40px').css('img::attr(src)').get()
         images = response.css('.z-1').css('img::attr(data-src)').getall()
         cards = response.css('.z-1')
-
         for i in range(len(cards) - 1):
             d = {"shop_img": shop_img, "device_img": images[i], "model": cards[i].css('.hov-text-primary::text').get(),
                  "price": [cards[i].css('.text-primary::text').get()[2:-1]], "link": cards[i].css('a::attr(href)').get()}
+            if cards[i].css('.mr-1::text').get():
+                d["price"].append(cards[i].css('.mr-1::text').get()[1:])
             product_list.append(d)
 
 
--- kry/kry/spiders/kryspider.py
+++ kry/kry/spiders/kryspider.py
@@ -4,42 +4,33 @@ from scrapy.crawler import CrawlerProcess
 product_list = []
 
 
-class Spider1(scrapy.Spider):
-    name = "gadgetNgadget"
+class Spider3(scrapy.Spider):
+    name = "gadgetMonkey"
 
     def __init__(self, arg):
         self.arg = arg
 
     def start_requests(self):
-        url = f"https://gngbd.com/product/search?search={self.arg}"
+        url = f"https://www.gadgetmonkeybd.com/search?keyword={self.arg}"
         yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
         global product_list
-        shop_img = response.css('.sticky_nav').css('img::attr(src)').get()
-        images = response.css('div.product-img img::attr(src)').getall()
-        models = response.css('.product-gallery-item-content h4::text').extract()
-        priceWithBugs = response.css('.product-gallery-item-content span::text').extract()
-        print(priceWithBugs)
-        prices = []
-        for i in range(0, len(priceWithBugs), 3):
-            prices.append(priceWithBugs[i][1:])
-        linksWithBugs = response.css('.product-gallery-item-content a').css('a::attr(href)').getall()
-        links = []
-        cards = response.css('.product-gallery-item-content')
-        for i in range(0, len(linksWithBugs), 2):
-            links.append(linksWithBugs[i])
-        for i, j in enumerate(cards):
-            d = {"shop_img": shop_img, "device_img": images[i], "model": j.css('h4::text').extract(),
-                 "price": [j.css('span::text').get()[1:]], "link": j.css('a').css('a::attr(href)').get()}
-            if j.css('del::text').get():
-                d["price"].append(j.css('del::text').get()[1:])
+        shop_img = response.css('.h-md-40px').css('img::attr(src)').get()
+        images = response.css('.z-1').css('img::attr(data-src)').getall()
+        cards = response.css('.z-1')
+        for i in range(len(cards) - 1):
+            d = {"shop_img": shop_img, "device_img": images[i], "model": cards[i].css('.hov-text-primary::text').get(),
+                 "price": [cards[i].css('.text-primary::text').get()[2:-1]], "link": cards[i].css('a::attr(href)').get()}
+            if cards[i].css('.mr-1::text').get():
+                d["price"].append(cards[i].css('.mr-1::text').get()[1:])
             product_list.append(d)
 
 
 q = "Samsung"
 process = CrawlerProcess()
-process.crawl(Spider1, q)
+process.crawl(Spider3, q)
 process.start()
 print(*product_list, sep='\n')
+print(len(product_list))
 # print(len(product_list))
